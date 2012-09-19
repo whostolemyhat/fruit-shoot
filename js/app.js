58b2f75e83b0519b73ccce8b7b41cfff314506b2
James Tease <jamestease@gmail.com>
2012-09-19 07:37:09 +0100

    Added cannon sprite and mouse co-ordinates

---
+++ js/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    var canvas = $('#canvas')[0];
+    var ctx = canvas.getContext('2d');
+
+    var cannon = new Image();
+    cannon.src = 'img/cannon.png';
+    cannon.onload = function() {
+        ctx.drawImage(cannon, 0, 0, cannon.width, cannon.height);
+    }
+
+    var score = 0;
+    var raspberries = 0;
+
+    function getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
+        }
+    }
+
+    canvas.addEventListener('mousemove', function(evt) {
+        var mousePos = getMousePos(canvas, evt);
+        console.log("Mouse: " + mousePos.x + ', ' + mousePos.y);
+    });
+    
+});
